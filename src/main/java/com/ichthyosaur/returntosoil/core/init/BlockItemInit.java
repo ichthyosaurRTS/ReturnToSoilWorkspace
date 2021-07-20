872f65e3db36aab681a5755965b1ae1e25450aa6
ruasoyhthci <50352447+ruasoyhthci@users.noreply.github.com>
2021-07-21 08:53:18 +1000

    Added a creative tab

--- src/main/java/com/ichthyosaur/returntosoil/core/init/BlockItemInit.java
+++ src/main/java/com/ichthyosaur/returntosoil/core/init/BlockItemInit.java
@@ -25,19 +25,19 @@ public class BlockItemInit {
 
     //Origin berry stuffs
     public static final Food ORIGIN_BERRY_FOOD = (new Food.Builder()).nutrition(2).saturationMod(0.1F).build();
-    public static final RegistryObject<Item> ORIGIN_BERRY_ITEM = ITEMS.register("origin_berry_item", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD).food(ORIGIN_BERRY_FOOD)));
+    public static final RegistryObject<Item> ORIGIN_BERRY_ITEM = ITEMS.register("origin_berry_item", () -> new Item(new Item.Properties().tab(ReturnToSoilItemGroup.RETURN_TO_SOIL).food(ORIGIN_BERRY_FOOD)));
     static Block originBerryBlock = new OriginBerryBlock(AbstractBlock.Properties.of(Material.PLANT, MaterialColor.COLOR_GREEN).noCollission().randomTicks().instabreak().sound(SoundType.GRASS));
     public static final RegistryObject<Block> ORIGIN_BERRY_BLOCK = BLOCKS.register("origin_berry_block", () -> originBerryBlock);
-    public static final RegistryObject<Item> ORIGIN_BERRY_SEED = ITEMS.register("origin_berry_seed", () -> new BlockNamedItem(originBerryBlock, new Item.Properties().tab(ItemGroup.TAB_FOOD)));
+    public static final RegistryObject<Item> ORIGIN_BERRY_SEED = ITEMS.register("origin_berry_seed", () -> new BlockNamedItem(originBerryBlock, new Item.Properties().tab(ReturnToSoilItemGroup.RETURN_TO_SOIL)));
 
 
     //lily lantern stuffs
     static Block lilypadLanternBlock = new LilypadLanternBlock(AbstractBlock.Properties.of(Material.PLANT, MaterialColor.COLOR_GREEN).noCollission().randomTicks().instabreak().sound(SoundType.GRASS).lightLevel(litBlockEmission(12)));
     public static final RegistryObject<Block> LILYPAD_LANTERN_BLOCK = BLOCKS.register("lilypad_lantern_block", () -> lilypadLanternBlock);
-    public static final RegistryObject<Item> LILYPAD_LANTERN_SEED = ITEMS.register("lilypad_lantern_seed", () -> new LilypadLanternSeed(lilypadLanternBlock, new Item.Properties().tab(ItemGroup.TAB_FOOD)));
+    public static final RegistryObject<Item> LILYPAD_LANTERN_SEED = ITEMS.register("lilypad_lantern_seed", () -> new LilypadLanternSeed(lilypadLanternBlock, new Item.Properties().tab(ReturnToSoilItemGroup.RETURN_TO_SOIL)));
 
 
-    public static Item roseBeetleItem= new Item(new Item.Properties().tab(ItemGroup.TAB_MISC));
+    public static Item roseBeetleItem= new Item(new Item.Properties().tab(ReturnToSoilItemGroup.RETURN_TO_SOIL));
     public static final RegistryObject<Item> ROSE_BEETLE_ITEM = ITEMS.register("rose_beetle_item", () -> roseBeetleItem);
 
     //public static final RegistryObject<Item> HUSK_BUG_MEAT_ITEM = ITEMS.register("husk_bug_meat_item", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
---
+++ src/main/java/com/ichthyosaur/returntosoil/core/init/ReturnToSoilItemGroup.java
@@ -0,0 +1,19 @@
+package com.ichthyosaur.returntosoil.core.init;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class ReturnToSoilItemGroup extends ItemGroup {
+
+    public static final ReturnToSoilItemGroup RETURN_TO_SOIL = new ReturnToSoilItemGroup(ItemGroup.getGroupCountSafe(),
+            "returntosoil");
+
+    public ReturnToSoilItemGroup(int index, String label) {
+        super(index, label);
+    }
+
+    @Override
+    public ItemStack makeIcon() {
+        return new ItemStack(BlockItemInit.ORIGIN_BERRY_SEED.get());
+    }
+}
