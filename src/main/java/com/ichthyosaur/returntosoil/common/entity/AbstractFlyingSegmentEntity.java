c2620695f010c00b99bae0949ec1e955861f079b
ruasoyhthci <50352447+ruasoyhthci@users.noreply.github.com>
2021-07-31 19:04:41 +1000

    Looks in correct directions now.

--- src/main/java/com/ichthyosaur/returntosoil/common/entity/AbstractFlyingSegmentEntity.java
+++ src/main/java/com/ichthyosaur/returntosoil/common/entity/AbstractFlyingSegmentEntity.java
@@ -79,9 +79,9 @@ public abstract class AbstractFlyingSegmentEntity extends MobEntity {
         if (this.getLeader()==null) this.kill();
         else {
             if (!this.getLeader().isAlive()) this.kill();
-        this.setDeltaMovement(this.getDeltaMovement().add(0, 0, 0)); //offset gravity
 
 
+            //START MOVEMENT
             double xPos = this.getX();
             double yPos = this.getY();
             double zPos = this.getZ();
@@ -99,6 +99,7 @@ public abstract class AbstractFlyingSegmentEntity extends MobEntity {
             double sqrFlatDistance = Math.sqrt(xDistance * xDistance + zDistance * zDistance);
 
 
+            //This should really be based on the parent so we can rotated based on their look direction but no worries for now.
             double flatRadianRotation = MathHelper.atan2(xDistance, zDistance);
             double flatDegreeRotation =  flatRadianRotation * (180F / (float) Math.PI);
 
@@ -145,115 +146,13 @@ public abstract class AbstractFlyingSegmentEntity extends MobEntity {
                 newX = parentX + (Math.cos(theta) * flatDistance);
             }
 
-
-
             this.moveTo(newX, newY, newZ);
+            //END OF MOVEMENT
 
+            //START OF ROTATION
+            this.setRot(((float) ((MathHelper.wrapDegrees(360-flatDegreeRotation)))),(float)(MathHelper.wrapDegrees(360-vertDegreeRotation)));
+            this.getLookControl().setLookAt(parentX, parentY, parentZ);
 
-
-        /*
-
-            double sqrFlatDistance = Math.sqrt(xDistance * xDistance + zDistance * zDistance);
-
-        //  gets angle in radians, converts to degrees
-        double wantedDistance = this.segmentSpaceFromLeader; //hypotenuse for us
-        double flatDegreeRotation = MathHelper.atan2(xDistance, zDistance) * (180F / (float) Math.PI);//
-        double yDegreeRotation = - MathHelper.atan2(yDistance, sqrFlatDistance) * (180F / (float) Math.PI); //apparently from -90 to 90... wtf
-            //if (yDistance<0) yDegreeRotation = -90 - yDegreeRotation;
-            //else yDegreeRotation = 90 - yDegreeRotation;
-            //sqr flat is always positive....
-
-            if (yDegreeRotation<0) {
-                if (this.getLeader().getDeltaMovement().y > 0) {yDegreeRotation+=10;} //moving up, looking down
-                else {yDegreeRotation-=10;}
-            }
-            else {
-                if (this.getLeader().getDeltaMovement().y > 0) {yDegreeRotation-=10;} //moving up, looking up
-                else {yDegreeRotation+=10;}
-            }
-
-            LOGGER.info("yDegreeRot = " + yDegreeRotation);
-
-        LOGGER.info(""+yDegreeRotation);
-
-        //  the up down rot here gets sent to the model anim, we need the 2nd float here and the setupAnim code to work. Also, miracle.
-        //  the entire segment model must be a child of fake head, or acting head.
-            //this.lookAt(this.getLeader(),100,100);
-            //this.getLookControl().setLookAt(parentX, parentY, parentZ);
-            //this.setRot(((float) ((MathHelper.wrapDegrees(360-flatDegreeRotation)))),(float)(MathHelper.wrapDegrees( yDegreeRotation)));
-            //this works dont touch unless you mess with ydegreerot as well
-
-            double sqrDistance = Math.sqrt(xDistance * xDistance + zDistance * zDistance + yDistance * yDistance);
-
-        // not accounting for 0,90,180,270 exact, using the 4 segment method sqrDistance > wantedDistance + 0.01 || sqrDistance < wantedDistance - 0.01 if (sqrDistance != wantedDistance)
-        if (sqrDistance > wantedDistance+1) {
-            double newZ;
-            double newX;
-            double newY;
-
-            //they needs to be in radians to use idiot
-
-            double vertDistance;
-            double absYDegreeRotation= Math.abs(yDegreeRotation);
-            double yRadianRotation = yDegreeRotation * ((float) Math.PI / 180F); //changed this now produces neg
-
-            vertDistance = Math.cos(yRadianRotation) * wantedDistance; // just changed this now   AHH IS IT COS OR SIN
-            double flatWantedDistance = Math.sin(yRadianRotation) * wantedDistance; //changed this and vert
-
-
-            //can he not move closer & thus down because of x and z ?
-            //look at vectors and manipulate the ydegree rotation a little base on that?
-
-            if (yDegreeRotation>=0) {
-                newY = parentY+vertDistance;
-            }
-            else {
-                newY = parentY+vertDistance;
-            }
-
-
-
-
-            //also needs to move based on look at
-            double positiveFlatRotation = flatDegreeRotation;
-
-            double absYDistance = Math.abs(yDistance);
-
-           //lets try calculating x and y first i guess?
-            //well when z pushes towards z it pushes the whole entity too so i shouldnt be surprised...?
-            //may need to consider y in a completely different light...
-
-
-
-            //wanted distance should be flatwanteddistance for 3d movement
-            if (positiveFlatRotation < -90) {
-                double theta = positiveFlatRotation * ((float) Math.PI / 180F);  //here we take away however many 90 degree factors we need
-                newZ = parentZ - (Math.cos(theta) * flatWantedDistance);
-                newX = parentX - (Math.sin(theta) * flatWantedDistance); //minus bc in this case, the parent is above/greater than
-            } else if (positiveFlatRotation >= -90 && positiveFlatRotation < 0) {
-                double theta = (positiveFlatRotation - 90)* ((float) Math.PI / 180F);
-                newZ = parentZ + (Math.sin(theta) * flatWantedDistance);
-                newX = parentX - (Math.cos(theta) * flatWantedDistance);
-            } else if (positiveFlatRotation >= 0 && positiveFlatRotation < 90) {
-                double theta = (positiveFlatRotation - 180)* ((float) Math.PI / 180F);
-                newZ = parentZ + (Math.cos(theta) * flatWantedDistance);
-                newX = parentX + (Math.sin(theta) * flatWantedDistance);
-            } else {
-                double theta = (positiveFlatRotation - 270)* ((float) Math.PI / 180F);
-                newZ = parentZ - (Math.sin(theta) * flatWantedDistance);
-                newX = parentX + (Math.cos(theta) * flatWantedDistance);
-            }
-
-
-            double newDistance = Math.sqrt((parentX-newX) * (parentX-newX) + (parentZ-newZ) * (parentZ-newZ) + (parentY-newY) * (parentY-newY));
-            LOGGER.info("NewDistanceCorrect: "+(newDistance<wantedDistance+0.1||newDistance>wantedDistance-0.1));
-            //LOGGER.info("NewDist: "+newDistance + "   WantedDist: "+wantedDistance);
-            //LOGGER.info("posFlatRot: "+positiveFlatRotation);
-            //LOGGER.info("absYRot: "+absYDegreeRotation);
-
-            this.moveTo(newX, newY, newZ);
-
-        } */
     }
     }
 
