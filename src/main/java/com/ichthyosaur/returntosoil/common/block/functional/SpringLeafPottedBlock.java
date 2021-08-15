e6057d41ca6b45a4b27a4f9b648148951670ed4f
ruasoyhthci <50352447+ruasoyhthci@users.noreply.github.com>
2021-08-16 03:22:35 +1000

    Added some spring leaf textures, drops

--- src/main/java/com/ichthyosaur/returntosoil/common/block/functional/SpringLeafPottedBlock.java
+++ src/main/java/com/ichthyosaur/returntosoil/common/block/functional/SpringLeafPottedBlock.java
@@ -4,10 +4,13 @@ import com.ichthyosaur.returntosoil.RTSMain;
 import com.ichthyosaur.returntosoil.core.init.TileEntityTypesInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -20,16 +23,20 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class SpringLeafPottedBlock extends Block {
 
     //remember these have to be psf or mod breaks~
-    public static final IntegerProperty ROTATION = BlockStateProperties.ROTATION_16;
+    public static final DirectionProperty FACING = BlockStateProperties.FACING;
     public static final IntegerProperty COOL_DOWN = RTSMain.COOL_DOWN;
 
     public SpringLeafPottedBlock(Properties p_i48440_1_) {
         super(p_i48440_1_);
-        this.registerDefaultState(this.stateDefinition.any().setValue(COOL_DOWN,0).setValue(ROTATION,0));
+        this.registerDefaultState(this.stateDefinition.any().setValue(COOL_DOWN,0).setValue(FACING, Direction.NORTH));
     }
 
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> p_206840_1_) {
-        p_206840_1_.add(COOL_DOWN,ROTATION);
+        p_206840_1_.add(COOL_DOWN, FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_) {
+        return this.defaultBlockState().setValue(FACING, p_196258_1_.getHorizontalDirection().getOpposite()).setValue(COOL_DOWN,0);
     }
 
     @Override
@@ -48,7 +55,8 @@ public class SpringLeafPottedBlock extends Block {
         super.onPlace(p_220082_1_, p_220082_2_, p_220082_3_, p_220082_4_, p_220082_5_);
     }
 
-    protected static final VoxelShape SHAPE = Block.box(6.0D, 2.0D, 6.0D, 6.0D, 16.0D, 6.0D);
+    //4 OFF THE SIDES, 2 OFF THE BOTTOM, HEIGHT 14
+    protected static final VoxelShape SHAPE = Block.box(4.0D, 2.0D, 4.0D, 12.0D, 14.0D, 12.0D);
     @ParametersAreNonnullByDefault
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
