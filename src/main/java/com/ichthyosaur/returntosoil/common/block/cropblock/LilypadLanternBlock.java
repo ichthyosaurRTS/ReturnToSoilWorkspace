4c16f8ed1baef86e1ce698f5ca9418634d188868
ruasoyhthci <50352447+ruasoyhthci@users.noreply.github.com>
2021-08-18 15:36:00 +1000

    Added canSurvive to lilypad lantern

--- src/main/java/com/ichthyosaur/returntosoil/common/block/cropblock/LilypadLanternBlock.java
+++ src/main/java/com/ichthyosaur/returntosoil/common/block/cropblock/LilypadLanternBlock.java
@@ -17,10 +17,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootContext;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -65,9 +68,7 @@ public class LilypadLanternBlock extends RTSCropsBlock{
                 float f = getGrowthSpeed(this, worldIn, pos);
                 if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, random.nextInt((int)(25.0F / f) + 1) == 0)) //that last bool is the grow chance
                 {
-                    if (worldIn.getBlockState(pos.below()).is(Blocks.FARMLAND)) {worldIn.removeBlock(pos,false);
-                        popResource(worldIn, pos, new ItemStack(BlockItemInit.LILYPAD_LANTERN_SEED.get(),1));} //destroy the block if on farmland
-                    else { worldIn.setBlock(pos, this.nextAgeWithRotationWithLit(state,i+1), 2);}
+                    worldIn.setBlock(pos, this.nextAgeWithRotationWithLit(state,i+1), 2);
                     net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state);
                 }
             }
@@ -77,6 +78,11 @@ public class LilypadLanternBlock extends RTSCropsBlock{
                 //else if (rollChance(300)) spawnBaruGaru(worldIn, pos);
             }
         }
+    }
+
+    public boolean canSurvive(BlockState state, IWorldReader worldReader, BlockPos pos) {
+        if (worldReader.getBlockState(pos.below()).is(Blocks.FARMLAND)) {return false;}
+        else return super.canSurvive(state,worldReader,pos);
     }
 
     @ParametersAreNonnullByDefault
