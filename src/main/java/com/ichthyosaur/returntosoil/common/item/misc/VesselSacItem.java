af41ef40886d823d943984b82634138cc6582744
ruasoyhthci <50352447+ruasoyhthci@users.noreply.github.com>
2022-01-17 00:53:05 +1100

    updated some textures

--- src/main/java/com/ichthyosaur/returntosoil/common/item/misc/VesselSacItem.java
+++ src/main/java/com/ichthyosaur/returntosoil/common/item/misc/VesselSacItem.java
@@ -4,6 +4,7 @@ import com.ichthyosaur.returntosoil.common.entity.VesselEntity;
 import com.ichthyosaur.returntosoil.common.item.abst.RTSDescItem;
 import com.ichthyosaur.returntosoil.core.init.EntityTypesInit;
 import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
@@ -54,6 +55,27 @@ public class VesselSacItem extends RTSDescItem {
 
         else  return ActionResult.fail(itemstack);
     }
+
+    /*@Override
+    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
+        World world = context.getLevel();
+        if (!world.isClientSide()) {
+
+            PlayerEntity player = context.getPlayer();
+            BlockPos pos = context.getClickedPos();
+            ItemStack itemstack = context.getItemInHand();
+
+            VesselEntity entity = EntityTypesInit.VESSEL.get().create(world);
+            entity.setOwner(player);
+            entity.moveTo((double) pos.getX() + 0.5, (double) pos.getY() + 1, (double) pos.getZ() + 0.5, 0.0F, 0.0F);
+            entity.finalizeSpawn((ServerWorld) world, world.getCurrentDifficultyAt(pos), SpawnReason.NATURAL, null, null);
+            world.addFreshEntity(entity);
+
+            itemstack.shrink(1);
+        }
+
+        return super.onItemUseFirst(stack, context);
+    }*/
 
     public ActionResultType useOn(ItemUseContext p_195939_1_) {
         return ActionResultType.PASS;
